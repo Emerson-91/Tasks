3fb3e06806051b4a8372fd0501f20cf8a0a19eee
Emerson Henrique <emerson.hcdossantos@gmail.com>
2023-05-14 10:34:29 -0400

    adicionado um getTasksList

---
+++ src/config/firebaseconfig.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+import {initializeApp} from 'firebase/app'
+import "firebase/storage"
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCrpRUvlL0y3wlPBQwMN9aF9FWsjtsZmcA",
+    authDomain: "task-2845f.firebaseapp.com",
+    projectId: "task-2845f",
+    storageBucket: "task-2845f.appspot.com",
+    messagingSenderId: "369229576381",
+    appId: "1:369229576381:web:13a8d1188df6c8629c9fd4"
+  };
+ 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = firebase.firestore();
+export default database
